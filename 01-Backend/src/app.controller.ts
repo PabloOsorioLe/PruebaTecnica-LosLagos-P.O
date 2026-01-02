7af61405f9b117a46288d38b6c932ab888234088
MARK01\pablo <pablo.osorio.le@gmail.com>
2026-01-02 16:38:12 -0300

    05-Push

--- 01-Backend/src/app.controller.ts
+++ 01-Backend/src/app.controller.ts
@@ -5,8 +5,19 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  // Este endpoint responderá en: GET /api
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  // NUEVO: Este endpoint responde: GET /api/healthz en cronJob
+  @Get('healthz')
+  checkHealth() {
+    return {
+      status: 'ok',
+      service: 'GrupoLagos-Backend',
+      timestamp: new Date().toISOString()
+    };
   }
 }
