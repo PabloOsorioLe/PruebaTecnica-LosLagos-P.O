cb0bc9077f2b0e47f7936fa8e347f00a76bc38f9
MARK01\pablo <pablo.osorio.le@gmail.com>
2026-01-02 14:36:56 -0300

    03-Push

--- 01-Backend/src/main.ts
+++ 01-Backend/src/main.ts
@@ -1,11 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
-
- async function bootstrap() {
+async function bootstrap() {
+  const logger = new Logger('Bootstrap');
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api'); 
-  app.enableCors(); 
-  await app.listen(3000);
+
+  // 1. Prefijo global para que los endpoints sean /api/...
+  app.setGlobalPrefix('api');
+
+  // 2. CORS: Permitimos solo a frontend de Vercel
+  app.enableCors({
+    origin: [
+      'http://localhost:4200', 
+      'https://prueba-tecnica-los-lagos-p-o.vercel.app' // URL que me enviaste
+    ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
+  // 3. PUERTO: Obligatorio para Render (usan variables de entorno)
+  const port = process.env.PORT || 3000;
+  
+  await app.listen(port);
+  logger.log(`Backend de GrupoLagos corriendo en el puerto: ${port}`);
 }
 bootstrap();
